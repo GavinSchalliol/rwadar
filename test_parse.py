3c11b330e6d42da3d8885ec7cb7af79e5fd24c1d
GavinSchalliol <gschalli@thoughtworks.com>
2016-02-13 19:10:26 +0100

    anonymized test file, sort of

--- test_parse.py
+++ test_parse.py
@@ -4,4 +4,4 @@ from pyquery import PyQuery as pq
 x = pq(filename="tests/data/5402101.html")
 
 def test_get_page_title():
-	assert getPageTitle(x) == "Zwischenmiete in alternativer WG - WG Zimmer in Luneburg-Luneburg"
+	assert getPageTitle(x) == "Title of the page"

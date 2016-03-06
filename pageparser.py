d4b674cbf60bc7262cf7347ac78ee0ed73f385ac
GavinSchalliol <gschalli@thoughtworks.com>
2016-03-06 15:40:55 +0100

    one test passes, booyah

--- pageparser.py
+++ pageparser.py
@@ -8,6 +8,10 @@ d = pq(url)
 class Listing:
 	def __init__(self,d):
 		pass
+	def title(self,d):
+		titleObj = [title.text for title in d('title')]
+		title = titleObj[0].encode('utf8')
+		return title
 	def city(self,d):
 		pass
 	def plz(self,d):

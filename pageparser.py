62dc6dbf0849a810df66cc57451a72e07396326c
GavinSchalliol <gschalli@thoughtworks.com>
2016-03-06 15:00:53 +0100

    set up scaffolding and failing tests

---
+++ pageparser.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from pyquery import PyQuery as pq
+from config import i, site, limit, incr
+
+class Listing():
+	def __init__(self):
+		pass
+	def city(self):
+		pass
+	def plz(self):
+		pass
+	def rent(self):
+		pass
+	def size(self):
+		pass
+
+def resultsString():
+	pass
+
+listing = Listing()
---
+++ test_pageparser.py
@@ -0,0 +1,22 @@
+# encoding=utf8  
+from pageparser import Listing
+from pyquery import PyQuery as pq
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+x = pq(filename="tests/data/5402101.html")
+
+def test_get_page_title():
+	assert listing.title(x) == "Title of the page"
+
+def test_get_page_rent_and_size():
+	assert listing.rent(x) == ("300€")
+	assert listing.size(x) == ("20m²")
+
+def test_get_page_city():
+	assert listing.city(x) == ("Lüneburg")
+	assert listing.plz(x) == ("21335")
+
+def test_export_of_joined_results():
+	assert resultsString() == "Title of the page;Lüneburg;21335;300€;20m²"

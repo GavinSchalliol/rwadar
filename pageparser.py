6596256ab3ed8123bc550b4cc71935cbcb422eec
GavinSchalliol <gschalli@thoughtworks.com>
2016-03-06 23:11:12 +0100

    it works! :shipit:

--- pageparser.py
+++ pageparser.py
@@ -2,48 +2,64 @@
 from pyquery import PyQuery as pq
 from config import i, site, limit, incr
 
-url = "http://" + site + "/" + str(i) + ".html"
-d = pq(url)
-
 class Listing:
 	def __init__(self,d):
 		pass
 	def title(self,d):
 		titleObj = [title.text for title in d('title')]
-		title = titleObj[0].encode('utf8')
-		return title
+		if titleObj:
+			title = titleObj[0].encode('utf8')
+			return str(title)
 	def city(self,d):
 		cityObj = d('div').filter('.col-sm-4')
 		cityObj = cityObj.text()
 		cityObj = cityObj.split()
-		city = cityObj[2]
-		return city
+		if cityObj:
+			city = cityObj[2].encode('utf8')
+			return str(city)
 	def plz(self,d):
 		cityObj = d('div').filter('.col-sm-4')
                 cityObj = cityObj.text()
                 cityObj = cityObj.split()
-                plz = cityObj[1]
-		return plz
+                if cityObj:
+			plz = cityObj[1]
+			return str(plz)
 	def rent(self,d):
 		rentObj = d('h1').filter('.headline-key-facts')
 		rentObj = rentObj.text()
 		if rentObj:
 			rentObj = rentObj.split()
 			rent = rentObj[3].encode('utf8')
-			return rent
+			return str(rent)
 	def size(self,d):
 		rentObj = d('h1').filter('.headline-key-facts')
                 rentObj = rentObj.text()
                 if rentObj:
                         rentObj = rentObj.split()
                         size = rentObj[1].encode('utf8')
-                        return size
-
-listing = Listing(d)
+                        return str(size)
+	def categorizer(self,d):
+		if "Suchkriterien" in d.text():
+			# This is a Gesuch
+			return 2
+		if self.title(d) and self.city(d) and self.plz(d) and self.rent(d) and self.size(d):
+			return 1
+		else:
+			return 0
 
 def resultsString(d):
-        resultsString = listing.title(d) + ";" + listing.city(d) + ";" + listing.plz(d) + ";" + listing.rent(d) + ";" + listing.size(d)
-        return resultsString
+	listing = Listing(d)
+	resultsString = listing.title(d) + ";" + listing.city(d) + ";" + listing.plz(d) + ";" + listing.rent(d) + ";" + listing.size(d)
+       	return resultsString
 
-
+if __name__ == '__main__':
+	file = open('results.txt', 'a')
+        while (i < limit):
+                page = ("http://" + site + "/" + str(i) + ".html")
+                i = i + 1
+		d = pq(url=page)
+		listing = Listing(d)
+		print page
+		if listing.categorizer(d) == 1:
+			file.write(resultsString(d))
 
